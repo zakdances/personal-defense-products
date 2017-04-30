c7976ba713ac08704ba9ed35ec103adf242a704b
Zac <zakdances@gmail.com>
2017-04-29 19:30:20 -0500

    added new hero

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,33 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'app works!';
+  title = "Kandi's Land";
+  content = [];
+  products = [
+    {
+      title: 'Taser',
+      price: 100,
+      img: 'assets/taser1.png'
+    },
+    {
+      title: 'Stun Gun',
+      price: 150,
+      img: 'assets/taser2.png'
+    },
+    {
+      title: 'Taser',
+      price: 65,
+      img: 'assets/taser1.png'
+    },
+    {
+      title: 'Taser',
+      price: 49,
+      img: 'assets/taser1.png'
+    },
+    {
+      title: 'Taser',
+      price: 129,
+      img: 'assets/taser1.png'
+    }
+  ];
 }
